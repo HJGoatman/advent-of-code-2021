c6e97a342e64d8f0a5b15bf25c28611e8c23ab29
HJGoatman <17580529+HJGoatman@users.noreply.github.com>
2021-12-01 22:46:38 +0000

    Part 2

--- 1/1.py
+++ 1/1.py
@@ -1,10 +1,14 @@
-with open('input.txt', 'r') as input_file:
+import sys
+
+with open(sys.argv[1], 'r') as input_file:
     data = [int(d) for d in input_file.read().split('\n') if d != '']
+
+data = [sum(data[i: i+3]) for i in range(len(data) - 2)]
 
 total_increasing = 0
 
 for i in range(0, len(data) - 1):
-    if (data[i + 1] >= data[i]):
+    if (data[i + 1] > data[i]):
         total_increasing = total_increasing + 1
 
 print(total_increasing)
