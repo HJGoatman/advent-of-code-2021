592f9372af4edce7a7b0eabfaa7fd7a26ad62e3f
HJGoatman <17580529+HJGoatman@users.noreply.github.com>
2021-12-04 11:58:24 +0000

    Day 4 Part 2

--- 4/4.py
+++ 4/4.py
@@ -15,16 +15,19 @@ parts = input.split("\n\n")
 numbers = [int(number) for number in parts[0].split(",")]
 boards = list(map(to_board, parts[1:]))
 
+scores = []
+
 for number in numbers:
     # Mark off matching number for each board.
     for i, board in enumerate(boards):
         boards[i][1] = board[1] | (board[0] == number)
 
     # Check if any rows or columns are all true.
-    for values, marked in boards:
+    for i, (values, marked) in enumerate(boards):
         if np.any(np.all(marked, axis=0)) or np.any(np.all(marked, axis=1)):
             # print(number, values, marked)
             score = np.sum(values[~marked]) * number
-            print(score)
-            exit()
-        
+            scores.append(score)
+            boards.pop(i)
+
+print(scores[-1])        
