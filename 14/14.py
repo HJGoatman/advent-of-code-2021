77fb8a01c7481093036522e46d9bfd3c96f145ab
HJGoatman <17580529+HJGoatman@users.noreply.github.com>
2021-12-14 10:07:03 +0000

    Day 14 Part 2

--- 14/14.py
+++ 14/14.py
@@ -18,15 +18,35 @@ insertion_rules = {
     ]
 }
 
+pair_counter = {match: 0 for match in insertion_rules.keys()}
+pairs = [template[j : j + 2] for j in range(len(template) - 1)]
+for pair in pairs:
+    pair_counter[pair] = pair_counter[pair] + 1
+
+
+def get_new_pairs(pair, amount):
+    new_value = insertion_rules[pair]
+    return {pair[0] + new_value: amount, new_value + pair[1]: amount}
+
+
+def combine_dicts(dict1, dict2):
+    return Counter(dict1) + Counter(dict2)
+
 
 for i in range(steps):
-    pairs = [template[j : j + 2] for j in range(len(template) - 1)]
+    pair_counter = reduce(
+        combine_dicts,
+        map(lambda x: get_new_pairs(*x), list(pair_counter.items())),
+    )
 
-    triples = list(map(lambda pair: pair[0] + insertion_rules[pair] + pair[1], pairs))
 
-    template = reduce(lambda x, y: x + y[:2], triples, "") + triples[-1][-1]
+letter_count = reduce(
+    combine_dicts,
+    [{key[1]: amount} for key, amount in pair_counter.items()],
+)
 
+letter_count[template[0]] = letter_count[template[0]] + 1
 
-count_values = Counter(template).values()
+count_values = letter_count.values()
 
 print(max(count_values) - min(count_values))
