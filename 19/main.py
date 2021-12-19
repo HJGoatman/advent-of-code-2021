f619cfefc034e2c00525b8f4d73c22d1fbd2c9e7
HJGoatman <17580529+HJGoatman@users.noreply.github.com>
2021-12-19 19:33:01 +0000

    Matched up beacons

--- 19/main.py
+++ 19/main.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from functools import reduce
 
 
 @dataclass
@@ -29,3 +30,89 @@ def read_scanner_report(scanner_report: str) -> ScannerReport:
 
 def read_scanner_reports(scanner_reports: str) -> list[ScannerReport]:
     return list(map(read_scanner_report, scanner_reports.split("\n\n")))
+
+
+def get_beacon_displacements(beacons: list[Beacon], accessor):
+    values = [accessor(beacon) for beacon in beacons]
+    return [
+        abs(value_1 - value_2)
+        for j, value_1 in enumerate(values)
+        for i, value_2 in enumerate(values)
+        if i != j and i < j
+    ]
+
+
+def combine_dicts(dict1, dict2):
+    return {
+        **{
+            k: (v.intersection(dict2[k]) if k in dict2 else v) for k, v in dict1.items()
+        },
+        **{k: v for k, v in dict2.items() if k not in dict1},
+    }
+
+
+def find_overlapping_beacons(
+    scanner_report_1: ScannerReport,
+    scanner_report_2: ScannerReport,
+    min_overlapping_beacons,
+) -> list[Beacon]:
+
+    matches = []
+
+    for accessor in [
+        lambda beacon: beacon.x,
+        lambda beacon: beacon.y,
+        lambda beacon: beacon.z,
+    ]:
+
+        displacements_1, displacements_2 = (
+            get_beacon_displacements(scanner_report.beacons, accessor)
+            for scanner_report in [scanner_report_1, scanner_report_2]
+        )
+
+        common_distances = set(displacements_1).intersection(set(displacements_2))
+
+        common_displacements_1, common_displacements_2 = (
+            [
+                (i, dist)
+                for i, dist in enumerate(displacements)
+                if dist in common_distances
+            ]
+            for displacements in [displacements_1, displacements_2]
+        )
+
+        matches.append(
+            {
+                i: [j for j, v2 in common_displacements_2 if v1 == v2]
+                for i, v1 in common_displacements_1
+            }
+        )
+
+    edge_match = {
+        i: reduce(
+            lambda list1, list2: set(list1).intersection(list2),
+            [match[i] for match in matches],
+        )
+        for i in matches[0]
+    }
+    if all([len(s) == 1 for _, s in edge_match.items()]):
+        edge_map = {k: next(iter(v)) for k, v in edge_match.items()}
+        edges_1 = [
+            (i, j)
+            for j in range(len(edge_match))
+            for i in range(len(edge_match))
+            if i != j and i < j
+        ]
+        edges_2 = [edges_1[edge_map[i]] for i, _ in enumerate(edges_1)]
+
+        node_matches = reduce(
+            combine_dicts,
+            [{i: {k, l}, j: {k, l}} for (i, j), (k, l) in zip(edges_1, edges_2)],
+        )
+
+        if all([len(s) == 1 for _, s in node_matches.items()]):
+            node_map = {k: next(iter(v)) for k, v in node_matches.items()}
+            if len(node_map) >= min_overlapping_beacons:
+                return node_map
+            else:
+                return {}
--- 19/main_test.py
+++ 19/main_test.py
@@ -1,11 +1,24 @@
-from main import read_scanner_reports, Beacon, ScannerReport
+from os import read
+from main import read_scanner_reports, Beacon, ScannerReport, find_overlapping_beacons
+
+with open("tests/test1.txt") as input_file:
+    test_1_str = input_file.read()
 
 
 def test_read_scanner_reports():
-    with open("tests/test1.txt") as input_file:
-        input_str = input_file.read()
-
-    assert read_scanner_reports(input_str) == [
+    assert read_scanner_reports(test_1_str) == [
         ScannerReport(0, [Beacon(0, 2, 0), Beacon(4, 1, 0), Beacon(3, 3, 0)]),
         ScannerReport(1, [Beacon(-1, -1, 0), Beacon(-5, 0, 0), Beacon(-2, 1, 0)]),
     ]
+
+
+def test_find_overlapping_beacons():
+    scanner_reports = read_scanner_reports(test_1_str)
+    assert (
+        find_overlapping_beacons(
+            scanner_report_1=scanner_reports[0],
+            scanner_report_2=scanner_reports[1],
+            min_overlapping_beacons=3,
+        )
+        == {2: 2, 0: 1, 1: 0}
+    )
