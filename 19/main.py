645450ef593d3eef19dfd59d72b87d2f95c0ae2c
HJGoatman <17580529+HJGoatman@users.noreply.github.com>
2021-12-25 22:07:00 +0000

    Day 19 Part 1

--- 19/main.py
+++ 19/main.py
@@ -1,6 +1,11 @@
+from collections import defaultdict
 from dataclasses import dataclass
 from functools import reduce
 from itertools import permutations, combinations, product
+from os import access
+from queue import PriorityQueue
+import numpy as np
+import sys
 
 
 @dataclass(frozen=True, eq=True)
@@ -83,83 +88,230 @@ def find_overlapping_beacons(
     scanner_report_1: ScannerReport,
     scanner_report_2: ScannerReport,
     min_overlapping_beacons: int,
-) -> list[Beacon]:
-    edge_pairs = product(
-        *[
-            [
-                Edge(id=i, start=start, end=end)
-                for i, (start, end) in enumerate(combinations(report.beacons, 2))
+):
+    for accessor, rotation, alt_scanner_report_2 in get_rotations(scanner_report_2):
+        edge_pairs = product(
+            *[
+                [
+                    Edge(id=i, start=start, end=end)
+                    for i, (start, end) in enumerate(combinations(report.beacons, 2))
+                ]
+                for report in [scanner_report_1, alt_scanner_report_2]
             ]
-            for report in [scanner_report_1, scanner_report_2]
-        ]
-    )
-
-    matching_edges = list(
-        filter(
-            have_same_distances,
-            edge_pairs,
         )
-    )
 
-    beacon_mapping = {}
-    for edge1, edge2 in matching_edges:
-        x1_displacement = edge1.start.x - edge1.end.x
-        y1_displacement = edge1.start.y - edge1.end.y
-        z1_displacement = edge1.start.z - edge1.end.z
+        matching_edges = list(
+            filter(
+                have_same_distances,
+                edge_pairs,
+            )
+        )
 
-        x2_displacement = edge2.start.x - edge2.end.x
-        y2_displacement = edge2.start.y - edge2.end.y
-        z2_displacement = edge2.start.z - edge2.end.z
+        beacon_mapping = {}
+        for edge1, edge2 in matching_edges:
+            x1_displacement = edge1.start.x - edge1.end.x
+            y1_displacement = edge1.start.y - edge1.end.y
+            z1_displacement = edge1.start.z - edge1.end.z
 
-        if (
-            x1_displacement == x2_displacement
-            and y1_displacement == y2_displacement
-            and z1_displacement == z2_displacement
+            x2_displacement = edge2.start.x - edge2.end.x
+            y2_displacement = edge2.start.y - edge2.end.y
+            z2_displacement = edge2.start.z - edge2.end.z
+
+            if (
+                x1_displacement == x2_displacement
+                and y1_displacement == y2_displacement
+                and z1_displacement == z2_displacement
+            ):
+                beacon_mapping[edge1.start] = edge2.start
+                beacon_mapping[edge2.start] = edge1.start
+                beacon_mapping[edge1.end] = edge2.end
+                beacon_mapping[edge2.end] = edge1.end
+            else:
+                beacon_mapping[edge1.end] = edge2.start
+                beacon_mapping[edge2.start] = edge1.end
+                beacon_mapping[edge1.start] = edge2.end
+                beacon_mapping[edge2.end] = edge1.start
+
+        displacements = [
+            (beacon1.x - beacon2.x, beacon1.y - beacon2.y, beacon1.z - beacon2.z)
+            for beacon1, beacon2 in beacon_mapping.items()
+            if beacon1 in scanner_report_1.beacons
+        ]
+
+        if (len(set(displacements)) != 1) or (
+            (len(beacon_mapping) / 2) < min_overlapping_beacons
         ):
-            beacon_mapping[edge1.start] = edge2.start
-            beacon_mapping[edge2.start] = edge1.start
-            beacon_mapping[edge1.end] = edge2.end
-            beacon_mapping[edge2.end] = edge1.end
+            continue
         else:
-            beacon_mapping[edge1.end] = edge2.start
-            beacon_mapping[edge2.start] = edge1.end
-            beacon_mapping[edge1.start] = edge2.end
-            beacon_mapping[edge2.end] = edge1.start
+            break
 
     if (len(beacon_mapping) / 2) >= min_overlapping_beacons:
-        return beacon_mapping
+        return accessor, rotation, displacements[0], beacon_mapping
     else:
-        return {}
+        return None
+
+
+def vector_less_than(vec1, vec2):
+    return (vec1[0] < vec2[0]) and (vec1[1] < vec2[1]) and (vec1[2] < vec2[2])
+
+
+def vector_add(vec1, vec2):
+    return (vec1[0] + vec2[0], vec1[1] + vec2[1], vec1[2] + vec2[2])
+
+
+# Python implementation to find the
+# shortest path in the graph using
+# dictionaries
+
+# Function to find the shortest
+# path between two nodes of a graph
+def BFS_SP(graph, start, goal):
+    explored = []
+
+    # Queue for traversing the
+    # graph in the BFS
+    queue = [[start]]
+
+    # If the desired node is
+    # reached
+    if start == goal:
+        # print("Same Node")
+        return
+
+    # Loop to traverse the graph
+    # with the help of the queue
+    while queue:
+        path = queue.pop(0)
+        node = path[-1]
+
+        # Condition to check if the
+        # current node is not visited
+        if node not in explored:
+            neighbours = graph[node]
+
+            # Loop to iterate over the
+            # neighbours of the node
+            for neighbour in neighbours:
+                new_path = list(path)
+                new_path.append(neighbour)
+                queue.append(new_path)
+
+                # Condition to check if the
+                # neighbour node is the goal
+                if neighbour == goal:
+                    # print("Shortest path = ", *new_path)
+                    return new_path
+            explored.append(node)
+
+    # Condition when the nodes
+    # are not connected
+    # print("So sorry, but a connecting" "path doesn't exist :(")
+    return
+
+
+def get_all_distances(edges):
+    adjacency_list = {node1: [] for node1, _ in edges.keys()}
+    for node1, node2 in edges.keys():
+        adjacency_list[node1] = adjacency_list[node1] + [node2]
+
+    return {
+        node: BFS_SP(adjacency_list, 0, node) for node, _ in edges.keys() if node != 0
+    }
+
+
+def transform_beacons(beacons, accessor, rotation, displacement):
+    accessor_mapping = {
+        "X": lambda beacon: beacon.x,
+        "Y": lambda beacon: beacon.y,
+        "Z": lambda beacon: beacon.z,
+    }
+
+    beacons = [
+        Beacon(
+            accessor_mapping[accessor[0]](beacon),
+            accessor_mapping[accessor[1]](beacon),
+            accessor_mapping[accessor[2]](beacon),
+        )
+        for beacon in beacons
+    ]
+
+    beacons = [
+        Beacon(
+            beacon.x * rotation[0],
+            beacon.y * rotation[1],
+            beacon.z * rotation[2],
+        )
+        for beacon in beacons
+    ]
+
+    beacons = [
+        Beacon(
+            beacon.x + displacement[0],
+            beacon.y + displacement[1],
+            beacon.z + displacement[2],
+        )
+        for beacon in beacons
+    ]
+
+    return beacons
 
 
 def create_map(scanner_reports: list[ScannerReport], min_overlapping_beacons):
-    the_map = set()
+    scanners = {}
+    for scanner_report_1, scanner_report_2 in permutations(scanner_reports, 2):
+        result = find_overlapping_beacons(
+            scanner_report_1, scanner_report_2, min_overlapping_beacons
+        )
 
-    beacon_map = find_overlapping_beacons(
-        scanner_reports[0], scanner_reports[1], min_overlapping_beacons
-    )
+        if not result:
+            continue
 
-    differences = [
-        (beacon1.x - beacon2.x, beacon1.y - beacon2.y, beacon1.z - beacon2.z)
-        for beacon1, beacon2 in beacon_map.items()
-        if beacon1 in scanner_reports[0].beacons
-    ]
+        accessor, rotation, displacement, beacon_mapping = result
 
-    if len(set(differences)) != 1:
-        return {}
+        scanners[(scanner_report_1.id, scanner_report_2.id)] = (
+            accessor,
+            rotation,
+            displacement,
+        )
 
-    x_diff, y_diff, z_diff = differences[0]
-    the_map.add(Scanner(0, 0, 0))
+    scanners = {
+        **{
+            (edge[0], edge[1]): scanners[(edge[1], edge[0])]
+            for edge, _ in scanners.items()
+        },
+        **{
+            (edge[1], edge[0]): scanners[(edge[0], edge[1])]
+            for edge, _ in scanners.items()
+        },
+    }
 
-    for beacon in scanner_reports[0].beacons:
-        the_map.add(beacon)
+    # print(scanners)
+    print(scanners)
+    paths = get_all_distances(scanners)
+    print(paths)
+    # print()
+    # print(paths)
+    # print()
+    # print(scanners)
+    all_beacons = [scanner_reports[0].beacons]
+    scanner_positions = [(0, 0, 0)]
+    for i in range(1, len(scanner_reports)):
+        path = list(reversed(paths[i]))
+        beacons = scanner_reports[i].beacons
+        scanner_position = Beacon(0, 0, 0)
+        for j in range(len(path) - 1):
+            accessor, rotation, displacement = scanners[(path[j], path[j + 1])]
+            beacons = transform_beacons(beacons, accessor, rotation, displacement)
+            scanner_position = transform_beacons(
+                [scanner_position], accessor, rotation, displacement
+            )[0]
 
-    the_map.add(Scanner(0 + x_diff, 0 + y_diff, 0 + z_diff))
+        scanner_positions.append(
+            (scanner_position.x, scanner_position.y, scanner_position.z)
+        )
+        all_beacons.append(beacons)
 
-    for beacon in scanner_reports[1].beacons:
-        the_map.add(Beacon(beacon.x + x_diff, beacon.y + y_diff, beacon.z + z_diff))
-
-    return the_map
+    return scanner_positions, set(reduce(list.__add__, all_beacons))
 
 
 def _get_rotation(
@@ -171,7 +323,7 @@ def _get_rotation(
         for beacon in scanner_report.beacons
     ]
 
-    return ScannerReport(scanner_report.id, beacons)
+    return (x_mul, y_mul, z_mul), ScannerReport(scanner_report.id, beacons)
 
 
 def _swap_columns(scanner_report: ScannerReport) -> list[ScannerReport]:
@@ -185,41 +337,58 @@ def _swap_columns(scanner_report: ScannerReport) -> list[ScannerReport]:
 
     for accessor in permutations(accessors, 3):
         reports.append(
-            ScannerReport(
-                scanner_report.id,
-                [
-                    Beacon(
-                        *map(lambda ac: ac(beacon), [access[1] for access in accessor])
-                    )
-                    for beacon in scanner_report.beacons
-                ],
+            (
+                tuple([label for label, _ in accessor]),
+                ScannerReport(
+                    scanner_report.id,
+                    [
+                        Beacon(
+                            *map(
+                                lambda ac: ac(beacon),
+                                [access[1] for access in accessor],
+                            )
+                        )
+                        for beacon in scanner_report.beacons
+                    ],
+                ),
             )
         )
 
     return reports
 
 
-def get_rotations(scanner_report: ScannerReport) -> list[ScannerReport]:
+def get_rotations(scanner_report):
     potential_reports = _swap_columns(scanner_report)
 
     rotations = [
         (
-            [report for i, report in enumerate(potential_reports) if i in [0, 3, 4]],
+            [report for i, (report) in enumerate(potential_reports) if i in [0, 3, 4]],
             [(1, 1, 1), (-1, -1, 1), (-1, 1, -1), (1, -1, -1)],
         ),
         (
-            [report for i, report in enumerate(potential_reports) if i in [1, 2, 5]],
+            [report for i, (report) in enumerate(potential_reports) if i in [1, 2, 5]],
             [(1, 1, -1), (1, -1, 1), (-1, 1, 1), (-1, -1, -1)],
         ),
     ]
 
     reports = [
         [
-            _get_rotation(report, x_mul, y_mul, z_mul)
+            (accessor, *_get_rotation(report, x_mul, y_mul, z_mul))
             for x_mul, y_mul, z_mul in rotation_set
-            for report in report_subset
+            for accessor, report in report_subset
         ]
         for report_subset, rotation_set in rotations
     ]
 
     return reports[0] + reports[1]
+
+
+if __name__ == "__main__":
+    with open(sys.argv[1], "r") as input_file:
+        input = input_file.read()
+
+    scanner_reports = read_scanner_reports(input)
+
+    scanners, beacons = create_map(scanner_reports, 12)
+
+    print(len(beacons))
--- 19/main_test.py
+++ 19/main_test.py
@@ -28,7 +28,7 @@ def test_find_overlapping_beacons():
         scanner_report_1=scanner_reports[0],
         scanner_report_2=scanner_reports[1],
         min_overlapping_beacons=3,
-    ) == {
+    )[3] == {
         Beacon(3, 3, 0): Beacon(-2, 1, 0),
         Beacon(0, 2, 0): Beacon(-5, 0, 0),
         Beacon(4, 1, 0): Beacon(-1, -1, 0),
@@ -39,12 +39,12 @@ def test_find_overlapping_beacons():
 
 
 def test_create_map():
-    assert create_map(scanner_reports, 3) == {
+    scanners, beacons = create_map(scanner_reports, 3)
+    assert scanners[1] == (5, 2, 0)
+    assert beacons == {
         Beacon(0, 2, 0),
         Beacon(4, 1, 0),
         Beacon(3, 3, 0),
-        Scanner(0, 0, 0),
-        Scanner(5, 2, 0),
     }
 
 
@@ -54,9 +54,10 @@ def test_get_rotations():
 
     scanner_reports = read_scanner_reports(input)
 
-    rotations = get_rotations(scanner_reports[0])
+    rotation_info = get_rotations(scanner_reports[0])
+    list_of_reports = [report for _, _, report in rotation_info]
 
-    assert all([report in rotations for report in scanner_reports[1:]])
+    assert all([report in list_of_reports for report in scanner_reports[1:]])
 
 
 def test_overlapping_beacons_2():
@@ -65,9 +66,11 @@ def test_overlapping_beacons_2():
 
     scanner_reports = read_scanner_reports(input)
 
-    beacon_mapping = find_overlapping_beacons(
+    accessor, rotation, displacement, beacon_mapping = find_overlapping_beacons(
         scanner_reports[0], scanner_reports[1], 12
     )
+
+    print(accessor, rotation, displacement, beacon_mapping)
 
     assert {
         beacon1
@@ -88,21 +91,100 @@ def test_overlapping_beacons_2():
         Beacon(-485, -357, 347),
     }
 
-    assert {
-        beacon1
-        for beacon1 in beacon_mapping.keys()
-        if beacon1 in scanner_reports[1].beacons
-    } == {
-        Beacon(686, 422, 578),
-        Beacon(605, 423, 415),
-        Beacon(515, 917, -361),
-        Beacon(-336, 658, 858),
-        Beacon(-476, 619, 847),
-        Beacon(-460, 603, -452),
-        Beacon(729, 430, 532),
-        Beacon(-322, 571, 750),
-        Beacon(-355, 545, -477),
-        Beacon(413, 935, -424),
-        Beacon(-391, 539, -444),
-        Beacon(553, 889, -390),
+    assert displacement == (68, -1246, -43)
+
+
+def test_get_list_of_beacons():
+    with open("tests/test3.txt") as input_file:
+        input = input_file.read()
+
+    scanner_reports = read_scanner_reports(input)
+
+    scanners, beacons = create_map(scanner_reports, 12)
+
+    assert scanners[1] == (68, -1246, -43)
+    assert scanners[4] == (-20, -1133, 1061)
+    assert scanners[2] == (1105, -1205, 1229)
+    assert scanners[3] == (-92, -2380, -20)
+    assert beacons == {
+        Beacon(-892, 524, 684),
+        Beacon(-876, 649, 763),
+        Beacon(-838, 591, 734),
+        Beacon(-789, 900, -551),
+        Beacon(-739, -1745, 668),
+        Beacon(-706, -3180, -659),
+        Beacon(-697, -3072, -689),
+        Beacon(-689, 845, -530),
+        Beacon(-687, -1600, 576),
+        Beacon(-661, -816, -575),
+        Beacon(-654, -3158, -753),
+        Beacon(-635, -1737, 486),
+        Beacon(-631, -672, 1502),
+        Beacon(-624, -1620, 1868),
+        Beacon(-620, -3212, 371),
+        Beacon(-618, -824, -621),
+        Beacon(-612, -1695, 1788),
+        Beacon(-601, -1648, -643),
+        Beacon(-584, 868, -557),
+        Beacon(-537, -823, -458),
+        Beacon(-532, -1715, 1894),
+        Beacon(-518, -1681, -600),
+        Beacon(-499, -1607, -770),
+        Beacon(-485, -357, 347),
+        Beacon(-470, -3283, 303),
+        Beacon(-456, -621, 1527),
+        Beacon(-447, -329, 318),
+        Beacon(-430, -3130, 366),
+        Beacon(-413, -627, 1469),
+        Beacon(-345, -311, 381),
+        Beacon(-36, -1284, 1171),
+        Beacon(-27, -1108, -65),
+        Beacon(7, -33, -71),
+        Beacon(12, -2351, -103),
+        Beacon(26, -1119, 1091),
+        Beacon(346, -2985, 342),
+        Beacon(366, -3059, 397),
+        Beacon(377, -2827, 367),
+        Beacon(390, -675, -793),
+        Beacon(396, -1931, -563),
+        Beacon(404, -588, -901),
+        Beacon(408, -1815, 803),
+        Beacon(423, -701, 434),
+        Beacon(432, -2009, 850),
+        Beacon(443, 580, 662),
+        Beacon(455, 729, 728),
+        Beacon(456, -540, 1869),
+        Beacon(459, -707, 401),
+        Beacon(465, -695, 1988),
+        Beacon(474, 580, 667),
+        Beacon(496, -1584, 1900),
+        Beacon(497, -1838, -617),
+        Beacon(527, -524, 1933),
+        Beacon(528, -643, 409),
+        Beacon(534, -1912, 768),
+        Beacon(544, -627, -890),
+        Beacon(553, 345, -567),
+        Beacon(564, 392, -477),
+        Beacon(568, -2007, -577),
+        Beacon(605, -1665, 1952),
+        Beacon(612, -1593, 1893),
+        Beacon(630, 319, -379),
+        Beacon(686, -3108, -505),
+        Beacon(776, -3184, -501),
+        Beacon(846, -3110, -434),
+        Beacon(1135, -1161, 1235),
+        Beacon(1243, -1093, 1063),
+        Beacon(1660, -552, 429),
+        Beacon(1693, -557, 386),
+        Beacon(1735, -437, 1738),
+        Beacon(1749, -1800, 1813),
+        Beacon(1772, -405, 1572),
+        Beacon(1776, -675, 371),
+        Beacon(1779, -442, 1789),
+        Beacon(1780, -1548, 337),
+        Beacon(1786, -1538, 337),
+        Beacon(1847, -1591, 415),
+        Beacon(1889, -1729, 1762),
+        Beacon(1994, -1805, 1792),
     }
+    assert len(beacons) == 79
