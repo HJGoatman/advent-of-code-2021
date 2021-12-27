c27a24bfc8478aa70e2c2fb06972f675cfc39b36
HJGoatman <17580529+HJGoatman@users.noreply.github.com>
2021-12-27 23:16:52 +0000

    Day 21 Part 1

--- 21/main.py
+++ 21/main.py
@@ -1,6 +1,7 @@
 import sys
 import re
 from dataclasses import dataclass
+from itertools import cycle
 
 
 @dataclass
@@ -23,8 +24,35 @@ def load_input(input: str) -> list[Player]:
     ]
 
 
+def play_round(die, board, player):
+    move = next(die) + next(die) + next(die)
+    player.position = player.position + move
+
+    player.score = player.score + board[(player.position - 1) % 10]
+
+
+def play_game(player1, player2):
+    die = cycle(range(1, 101))
+    board = list(range(1, 11))
+
+    counter = 0
+    player1_turn = True
+    while player1.score < 1000 and player2.score < 1000:
+        if player1_turn:
+            play_round(die, board, player1)
+        else:
+            play_round(die, board, player2)
+
+        player1_turn = not player1_turn
+
+        counter = counter + 3
+
+    return counter * min(player1.score, player2.score)
+
+
 if __name__ == "__main__":
     with open(sys.argv[1]) as input_file:
         input_str = input_file.read()
 
     players = load_input(input_str)
+    print(play_game(*players))
--- 21/main_test.py
+++ 21/main_test.py
@@ -1,4 +1,4 @@
-from main import Player, load_input
+from main import Player, load_input, play_round, play_game
 
 with open("tests/test1.txt") as test_file:
     test_str = test_file.read()
@@ -9,3 +9,34 @@ def test_load_input():
         Player(1, 4, 0),
         Player(2, 8, 0),
     ]
+
+
+def test_play_round():
+    die = iter(range(1, 101))
+    board = list(range(1, 11))
+    player1, player2 = load_input(test_str)
+
+    play_round(die, board, player1)
+    assert player1.score == 10
+    play_round(die, board, player2)
+    assert player2.score == 3
+    play_round(die, board, player1)
+    assert player1.score == 14
+    play_round(die, board, player2)
+    assert player2.score == 9
+    play_round(die, board, player1)
+    assert player1.score == 20
+    play_round(die, board, player2)
+    assert player2.score == 16
+    play_round(die, board, player1)
+    assert player1.score == 26
+    play_round(die, board, player2)
+    assert player2.score == 22
+
+
+def test_play_game():
+    player1, player2 = load_input(test_str)
+    rounds = play_game(player1, player2)
+    print(rounds)
+
+    assert rounds * min(player1.score, player2.score) == 739785
